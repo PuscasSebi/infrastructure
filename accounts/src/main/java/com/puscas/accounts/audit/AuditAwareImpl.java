a3fa7e5ab89e412103c9942f1744b530d621934e
mihaela <mihaelamocan19@gmail.com>
2025-03-27 14:45:27 +0200

    Changes

---
+++ accounts/src/main/java/com/puscas/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,24 @@
+package com.puscas.accounts.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    /**
+     * Returns the current auditor of the application.
+     *
+     * @return the current auditor.
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNTS_MS");
+    }
+    //created by and updated by used this hardcodede value
+    //will be updated later
+
+	
+}
