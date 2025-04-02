d89f283ea90b3812ec5c8e03b3a1c3ae8d210213
mihaela <mihaelamocan19@gmail.com>
2025-04-02 12:20:17 +0300

    docker compose and jib

--- accounts/src/main/java/com/puscas/accounts/Demo.java
+++ accounts/src/main/java/com/puscas/accounts/Demo.java
@@ -2,9 +2,9 @@ package com.puscas.accounts;
 
 public class Demo {
 
-    public static void main(String[] args) {
+ /*   public static void main(String[] args) {
         System.out.println("Hello World");
-    }
+    }*/
 
     /**
      * Adds two numbers together.
