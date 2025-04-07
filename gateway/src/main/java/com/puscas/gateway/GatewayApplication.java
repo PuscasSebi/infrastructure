0424ee1c8b6b475a62acdbcf2e344a2003c57c76
mihaela <mihaelamocan19@gmail.com>
2025-04-07 15:43:52 +0300

    retry pattern

--- gateway/src/main/java/com/puscas/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/puscas/gateway/GatewayApplication.java
@@ -12,6 +12,7 @@ import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
@@ -39,7 +40,13 @@ public class GatewayApplication {
 				.route(p -> p
 						.path("/eazybank/loans/**")
 						.filters( f -> f.rewritePath("/eazybank/loans/(?<segment>.*)","/${segment}")
-								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+								.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+								.retry(retryConfig -> {
+									retryConfig.setRetries(3).setMethods(HttpMethod.GET, HttpMethod.POST)
+											.setBackoff(Duration.ofMillis(100), Duration.ofMillis(1000), 2, true);
+								})
+						)
+
 						.uri("lb://LOANS"))
 				.route(p -> p
 						.path("/eazybank/cards/**")
